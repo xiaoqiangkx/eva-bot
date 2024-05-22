813447fd69e4ed2527a8980b0997a9a7911db612
icarus <317164583@qq.com>
2024-05-22 13:48:55 +0800

    update

---
+++ datasets/utils/data_utills.py
@@ -0,0 +1,13 @@
+# encoding: utf-8
+import pandas as pd
+
+
+def main():
+    # Load the data
+    data = pd.read_csv('datasets/data/iris.csv')
+
+    return
+
+
+if __name__ == '__main__':
+    main()
