1e9eba34690abf2ffc6dc50d3adf6d63e5c0263f
icarus <317164583@qq.com>
2024-05-23 17:13:19 +0800

    add playground

---
+++ client.py
@@ -0,0 +1,9 @@
+# encoding: utf-8
+from langserve import RemoteRunnable
+
+remote_chain = RemoteRunnable("http://localhost:8000/agent/")
+
+remote_chain.invoke({
+    "input": "how can langsmith help with testing?",
+    "chat_history": []  # Providing an empty list as this is the first call
+})
