2085dfe698a4223e48a3363cf196c928b158e75f
icarus <317164583@qq.com>
2024-06-03 15:45:01 +0800

    add pipeline test

--- pipeline/test_custom_document_loader.py
+++ pipeline/test_custom_document_loader.py
@@ -3,11 +3,14 @@
 Customer Document Loader used for langchain
 """
 from pathlib import Path
+import logging
 import pytest
 from pipeline.custom_document_loader import CustomDocumentLoader
 from utils import consts
 
 TEST_DATA_DIR = Path(consts.ROOT_DIR) / "datasets" / "tests"
+
+logger = logging.getLogger(__name__)
 
 
 class TestCustomDocumentLoader:
@@ -15,8 +18,8 @@ class TestCustomDocumentLoader:
         Test the CustomDocumentLoader
     """
     @pytest.mark.parametrize("file_paths", [[
-        str(TEST_DATA_DIR / "20191106-蓝城·春风燕语立冬海报文案.docx"),
-        str(TEST_DATA_DIR / "20200110-蓝城·春风燕语价值海报.docx"),
+        str(TEST_DATA_DIR / "a.docx"),
+        str(TEST_DATA_DIR / "b.docx"),
     ]])
     def test_custom_document_loader(self, file_paths):
         """
@@ -25,9 +28,10 @@ class TestCustomDocumentLoader:
         custom_document_loader = CustomDocumentLoader(file_paths)
         documents = list(custom_document_loader.lazy_load())
         assert len(documents) == 2
-        # assert documents[0].page_content == "This is a test document"
-        # assert documents[1].page_content == "This is another test document"
+        logging.info(documents[0].page_content)
+        assert documents[0].page_content == "价值海报文案\n一方轩阔明朗地下室\n向下生长的深度修为\n延展居者生活的层次"
+        assert documents[1].page_content == "logo\n暖阳踏着青黄的韵脚\n捎来新冬的消息"
         assert documents[0].metadata["source"] == str(TEST_DATA_DIR /
-                                                      "20191106-蓝城·春风燕语立冬海报文案.docx")
+                                                      "a.docx")
         assert documents[1].metadata["source"] == str(TEST_DATA_DIR /
-                                                      "20200110-蓝城·春风燕语价值海报.docx")
+                                                      "b.docx")
