25d380d255be0e2dda4962f132710707d35a4885
icarus <317164583@qq.com>
2024-06-03 10:42:13 +0800

    add pipeline

---
+++ pipeline/partition.py
@@ -0,0 +1,19 @@
+# encoding: utf-8
+"""
+Partitioning the data into chunks for training and testing sets
+"""
+
+
+class Patition:
+    """_summary_
+    """
+
+    def __init__(self, data, test_size=0.2):
+        self.data = data
+        self.test_size = test_size
+
+    def part(self):
+        pass
+
+    def chunk(self):
+        pass
